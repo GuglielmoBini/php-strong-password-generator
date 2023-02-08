72cdfed4d2f118aae666d7bbbc2b17fdd6f6473f
GuglielmoBini <guglielmobini@gmail.com>
2023-02-08 19:00:25 +0100

    created functions.php for logic, added comments

---
+++ includes/functions.php
@@ -0,0 +1,24 @@
+<?php
+// array dei caratteri
+$characters = [
+    'abcdefghijklmnopqrstuvwxyz',
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    '0123456789',
+    '!$%&'
+];
+// funzione per creare una password randomica
+function create_random_password($length, $characters){
+    if (!$length || $length < 8 || $length > 16) return;
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
+    // variabile per ricavare random index dell'array    
+    $random_index = rand(0,3);
+    // variabile per calcolare la lunghezza di una stringa dell'array
+    $last_character_index = strlen($characters[$random_index]) - 1;
+    // prendo randomicamente una delle lettere/numeri/segni e li monto per creare la password
+    $password .= $characters[$random_index][rand(0,$last_character_index)];
+    };
+    return $password;
+};
+
+$generated_password = create_random_password($password_length, $characters);
--- index.php
+++ index.php
@@ -1,29 +1,13 @@
 <?php
+// numero per lumghezza password
 $password_length = $_GET['number'] ?? '';
-
-$characters = [
-    'abcdefghijklmnopqrstuvwxyz',
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-    '0123456789',
-    '!$%&'
-];
-
-function create_random_password($length, $characters){
-    if (!$length || $length < 8 || $length > 16) return;
-    $password = '';
-    for ($i = 0; $i < $length; $i++) {
-    $random_index = rand(0,3);
-    $last_character_index = strlen($characters[$random_index]) - 1;
-    $password .= $characters[$random_index][rand(0,$last_character_index)];
-    };
-    return $password;
-};
-
-$generated_password = create_random_password($password_length, $characters) ;
+// includes
+include './includes/functions.php'
 ?>
 
 <!DOCTYPE html>
 <html lang="en" data-bs-theme="dark">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -31,11 +15,13 @@ $generated_password = create_random_password($password_length, $characters) ;
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Strong Password generator</title>
 </head>
+
 <body>
     <div class='container text-center mt-5'>
         <h1 class='my-4'>Strong Password Generator</h1>
         <h3 class='my-3'>Genera una password sicura</h3>
         <p class='text-success'>La tua password è : <span class='fs-3'><?php echo $generated_password ?></span></p>
+        <!-- form -->
         <form action="" method='GET' class='d-flex flex-column align-items-center'>
             <div class="my-4 w-25 ">
                 <label for="passwordlength" class="form-label">Lunghezza password:</label>
@@ -48,4 +34,5 @@ $generated_password = create_random_password($password_length, $characters) ;
         </form>
     </div>
 </body>
+
 </html>
